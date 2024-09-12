1a99aa870786235a864c5b72d872131d3143e1de
kevintimachy <kevintimachy@gmail.com>
2024-09-12 00:21:41 -0400

    "Updated login and register page Card styling"

--- pages/login.js
+++ pages/login.js
@@ -1,4 +1,4 @@
-import { Card, Form, Alert, Button } from "react-bootstrap";
+import { Card, Form, Alert, Button, Container, Row, Col } from "react-bootstrap";
 import { useState } from 'react';
 import { authenticateUser } from '@/lib/authenticate';
 import { useRouter } from 'next/router';
@@ -34,22 +34,34 @@ export default function Login(props) {
 
     return (
         <>
-            <Card bg="light">
-                <Card.Body><h2>Login</h2>Enter your login information below:</Card.Body>
-            </Card>
             <br />
-            <Form onSubmit={handleSubmit}>
-                <Form.Group>
-                    <Form.Label>User:</Form.Label><Form.Control type="text" value={user} id="userName" name="userName" onChange={e => setUser(e.target.value)} />
-                </Form.Group>
-                <br />
-                <Form.Group>
-                    <Form.Label>Password:</Form.Label><Form.Control type="password" value={password} id="password" name="password" onChange={e => setPassword(e.target.value)} />
-                </Form.Group>
-                {warning && (<><br /><Alert variant="danger">{warning}</Alert></>)}
-                <br />
-                <Button variant="primary" className="pull-right" type="submit">Login</Button>
-            </Form>
+            <Container fluid>
+                <Row>
+                    <Col md={8} lg={6} className="mx-auto">
+
+                        <Card bg="light">
+                            <Card.Body><h2
+                                className="text-center">Login</h2><p className="text-center">Enter your login information below:</p>
+                                <hr />
+                                <Form onSubmit={handleSubmit}>
+                                    <Form.Group>
+                                        <Form.Label>User:</Form.Label><Form.Control type="text" value={user} id="userName" name="userName" onChange={e => setUser(e.target.value)} />
+                                    </Form.Group>
+                                    <br />
+                                    <Form.Group>
+                                        <Form.Label>Password:</Form.Label><Form.Control type="password" value={password} id="password" name="password" onChange={e => setPassword(e.target.value)} />
+                                    </Form.Group>
+                                    {warning && (<><br /><Alert variant="danger">{warning}</Alert></>)}
+                                    <br />
+                                    <Button variant="primary" className="pull-right w-100" type="submit">Login</Button>
+                                </Form>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
+            </Container>
+
+
         </>
     );
 }
--- pages/register.js
+++ pages/register.js
@@ -1,4 +1,4 @@
-import { Card, Form, Alert, Button } from "react-bootstrap";
+import { Card, Form, Alert, Button, Container, Row, Col } from "react-bootstrap";
 import { useState } from 'react';
 import { registerUser } from '@/lib/authenticate';
 import { useRouter } from 'next/router';
@@ -26,26 +26,61 @@ export default function Register(props) {
 
     return (
         <>
-            <Card bg="light">
-                <Card.Body><h2>Register</h2>Register for an account:</Card.Body>
-            </Card>
             <br />
-            <Form onSubmit={handleSubmit}>
-                <Form.Group>
-                    <Form.Label>User:</Form.Label><Form.Control type="text" value={user} id="userName" name="userName" onChange={e => setUser(e.target.value)} />
-                </Form.Group>
-                <br />
-                <Form.Group>
-                    <Form.Label>Password:</Form.Label><Form.Control type="password" value={password} id="password" name="password" onChange={e => setPassword(e.target.value)} />
-                </Form.Group>
-                <br />
-                <Form.Group>
-                    <Form.Label>Confirm Password:</Form.Label><Form.Control type="password" value={password2} id="password2" name="password2" onChange={e => setPassword2(e.target.value)} />
-                </Form.Group>
-                {warning && (<><br /><Alert variant="danger">{warning}</Alert></>)}
-                <br />
-                <Button variant="primary" className="pull-right" type="submit">Register</Button>
-            </Form>
+            <Container fluid>
+                <Row>
+                    <Col md={8} lg={6} className="mx-auto">
+                        <Card bg="light">
+                            <Card.Body>
+                                <h2 className="text-center">Register</h2>
+                                <p className="text-center">Register for an account:</p>
+                                <hr />
+                                <Form onSubmit={handleSubmit}>
+                                    <Form.Group className="mb-3">
+                                        <Form.Label>User:</Form.Label>
+                                        <Form.Control
+                                            type="text"
+                                            value={user}
+                                            id="userName"
+                                            name="userName"
+                                            onChange={e => setUser(e.target.value)}
+                                        />
+                                    </Form.Group>
+                                    <Form.Group className="mb-3">
+                                        <Form.Label>Password:</Form.Label>
+                                        <Form.Control
+                                            type="password"
+                                            value={password}
+                                            id="password"
+                                            name="password"
+                                            onChange={e => setPassword(e.target.value)}
+                                        />
+                                    </Form.Group>
+                                    <Form.Group className="mb-3">
+                                        <Form.Label>Confirm Password:</Form.Label>
+                                        <Form.Control
+                                            type="password"
+                                            value={password2}
+                                            id="password2"
+                                            name="password2"
+                                            onChange={e => setPassword2(e.target.value)}
+                                        />
+                                    </Form.Group>
+                                    {warning && (
+                                        <Alert variant="danger" className="mb-3">
+                                            {warning}
+                                        </Alert>
+                                    )}
+                                    <Button variant="primary" type="submit" className="w-100">
+                                        Register
+                                    </Button>
+                                </Form>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
+            </Container>
+
         </>
     );
 }
